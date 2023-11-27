9237728a498486d35d28fe0bd641a31cec17a53e
marvertid <marvertid@proton.me>
2023-11-27 09:53:44 +0100

    Esercizio 66 e 67 iniziato

--- Javascript/Esercizio 66/script.js
+++ Javascript/Esercizio 66/script.js
@@ -8,11 +8,11 @@ MARVERTI DIEGO 4AI*/
 
 
 let output = document.getElementById("output");
-
+let seasonOutput = document.getElementById("seasonOutput");
 function validate() {
-    let inputGiorno = document.getElementById("inputGiorno").value;
-    let inputMese = document.getElementById("inputMese").value;
-    if ((inputMese == "Febbraio" && inputGiorno > 28) || ((inputMese == "Aprile" || inputMese == "Giugno" || inputMese == "Settembre" || inputMese == "Novembre") && inputGiorno == 31)) {
+    let inputGiorno = Number.parseInt(document.getElementById("inputGiorno").value);
+    let inputMese = Number.parseInt(document.getElementById("inputMese").value);
+    if ((inputMese == 2 && inputGiorno > 28) || ((inputMese == 4 || inputMese == 6 || inputMese == 9 || inputMese == 11) && inputGiorno == 31)) {
         output.textContent = "Data non valida!";
         output.style.color = "red";
     }else {
@@ -23,5 +23,23 @@ function validate() {
 }
 
 function seasonFinder() {
-    
+    let inputGiorno = document.getElementById("inputGiorno").value;
+    let inputMese = document.getElementById("inputMese").value;
+    if (inputMese >= 3 && inputMese <= 6) {
+        if ((inputMese == 3 && inputGiorno >= 20) || (inputMese > 3 && inputMese < 6) || (inputMese == 6 && inputGiorno <= 20)) {
+            seasonOutput.textContent = "Primavera";
+        }
+    } else if (inputMese >= 6 && inputMese <= 9) {
+        if ((inputMese == 6 && inputGiorno >= 21) || (inputMese > 6 && inputMese < 9) || (inputMese == 9 && inputGiorno <= 22)) {
+            seasonOutput.textContent = "Estate";
+        }
+    } else if (inputMese >= 9 && inputMese <= 12) {
+        if ((inputMese == 9 && inputGiorno >= 23) || (inputMese > 9 && inputMese < 12) || (inputMese == 12 && inputGiorno <= 21)) {
+            seasonOutput.textContent = "Autunno";
+        }
+    } else {
+        seasonOutput.textContent = "Inverno"
+    }
 }
+
+//da risolvere
---
+++ Javascript/Esercizio 67/script.js
@@ -0,0 +1,10 @@
+/*Scrivere una funzione che permetta di calcolare la media geometrica fra 2, 3, 4 o 5 valori positivi;
+la media geometrica di n termini è la radice n-esima del prodotto degli n valori
+MARVERTI DIEGO 4AI*/
+
+let inputViewer = document.getElementById("inputViewer");
+let output = document.getElementById("output");
+
+function add() {
+    let input = 
+}
