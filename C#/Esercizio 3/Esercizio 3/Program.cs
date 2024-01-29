ceed710929b025c8d9e3cd145bf5cbbeb194fa17
marvertid <marvertid@proton.me>
2024-01-29 09:56:51 +0100

    Esercizi C#

---
+++ C#/Esercizio 3/Esercizio 3/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Scrivere un programma che calcoli la somma di n valori inseriti dall’utente
+ * MARVERTI DIEGO 4AI
+ */
+
+namespace Esercizio_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserire numero di valori da sommare: ");
+            int n;
+            int.TryParse(Console.ReadLine(), out n);
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Inserire il valore: ");
+                int input;
+                int.TryParse(Console.ReadLine(), out input);
+                sum += input;
+            }
+            Console.WriteLine("Somma: " + sum);
+        }
+    }
+}
