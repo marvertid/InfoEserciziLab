4df04ae6bd986511b587db3874da366d6c0b92ab
Marverti Diego <152005949+marvertid@users.noreply.github.com>
2024-10-05 09:47:12 +0200

    esercizi modulo 1

--- PHP/esercizio012_MARVERTI.php
+++ PHP/esercizio012_MARVERTI.php
@@ -14,7 +14,18 @@ Utilizzare il valore di π ottenuto per calcolare area e circonferenza di un cer
 		<h1>Esercizio 12</h1>
 		<h2>Marverti Diego</h2>
 		<?php
-		
+		$piGrecoCalcolato = (1/(1*2*3) - 1/(2*3*5) + 1/(3*4*7) - 1/(4*5*9)  + 1/(5 * 6 * 11) - 1/(6 * 7 * 13) + 1/(7 * 8 * 15)) + 3;
+
+		echo "π calcolato: " . $piGrecoCalcolato . "<br />";
+		echo "π: " . pi() . "<br />";
+
+		$raggio = random_int(1, 10);
+		$circonferenza = 2*pi()*$raggio;
+		$area = pow($raggio, 2)*pi();
+
+		echo "<b>Raggio: </b>" . $raggio . "<br />";
+		echo "<b>Circonferenza: </b>" . $circonferenza . "<br />";
+		echo "<b>Area: </b>" . $area;
 		?>
 		
 	</body>
