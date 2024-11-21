9d218ea5ca37579dc5205a7b8c0e80ffe0ca157f
Marverti Diego <152005949+marvertid@users.noreply.github.com>
2024-11-21 12:54:57 +0100

    esercizio

--- PHP/esercizio047_MARVERTI.php
+++ PHP/esercizio047_MARVERTI.php
@@ -12,7 +12,31 @@ essi (boolean, integer, float o string)
 		<h1>Esercizio 47</h1>
 		<h2>Marverti Diego</h2>
 		<?php
-			
+			function findType($var) {
+				if (is_bool($var)) {
+					return "Boolean";
+				} else 
+
+				switch ($var) {
+					case is_bool($var): return "Boolean";
+					case is_integer($var): return "Integer";
+					case is_float($var): return "Float";
+					case is_string($var): return "String";
+				}
+			}
+
+			function findArrayTypes($array) {
+				$output = " [";
+				foreach ($array as $value) {
+					$output .= " " . findType($value) ." ,";
+				}
+				$output .= " ]";
+
+				return $output;
+			}
+
+			$array = ["ciao", 47, true, false, 33, 2.1, "22", "bella"];
+			echo findArrayTypes($array);
 		?>
 	</body>
 </html>
