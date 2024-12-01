c4a0fda73c5737cf1cfa59834c5d5e38bed37723
Marverti Diego <152005949+marvertid@users.noreply.github.com>
2024-12-01 18:17:33 +0100

    esercizio modulo 4 completato

--- PHP/esercizio061_MARVERTI/ricezione.php
+++ PHP/esercizio061_MARVERTI/ricezione.php
@@ -17,28 +17,41 @@ inoltre, la classe di appartenenza -->
 			$arrayS = explode(".", $_POST["inputIP"]);
 				if (count($arrayS) == 4) {
 					foreach ($arrayS as $number) {
-						if (is_numeric($number) && $number < 255) {
+						if (is_numeric($number) && $number <= 255) {
 							return true;
 						}
 					}
 				}
 				return false;
 		}
+
+		function IpClass($ip): string {
+			$arrayS = explode(".", $ip);
+					$convertedBase = base_convert($arrayS[0], 10, 2);
+					$convertedBase = strrev($convertedBase);
+					while(strlen($convertedBase) < 8) {
+						$convertedBase .= "0";
+					}
+					$convertedBase = strrev($convertedBase);
+					$arrayExplodedBase = str_split($convertedBase);
+					return match (true) {
+						implode(array_chunk($arrayExplodedBase, 4)[0]) == "1111" => "CLASSE E",
+						implode(array_chunk($arrayExplodedBase, 4)[0]) == "1110" => "CLASSE D",
+						implode(array_chunk($arrayExplodedBase, 3)[0]) == "110" => "CLASSE C",
+						implode(array_chunk($arrayExplodedBase, 2)[0]) == "10" => "CLASSE B",
+						$arrayExplodedBase[0] == "0" => "CLASSE A",
+						default => null,
+					};
+		}
+
 		if ($_SERVER["REQUEST_METHOD"] === "POST") {
 			if(empty($_POST["inputIP"])) {
 				echo "<p><span style=color:red>Non hai inserito nessun valore!</span></p>";
 			} else {
 				if(isValidIP($_POST["inputIP"])) {
-					$arrayS = explode(".", $_POST["inputIP"]);
-					$convertedBase = base_convert($arrayS[0], 10, 2);
-					$arrayExplodedBase = str_split($convertedBase);
-					if ($arrayExplodedBase[0] == 0) {
-						echo "CLASSE A";
-					} else {
-						echo "suca";
-					}
+					echo "<p>" , IpClass($_POST["inputIP"]) , "</p>";
 				} else {
-					echo "test";
+					echo "<p><span style=color:red>Non hai inserito un indirizzo ip valido!</span></p>";
 				}
 			}
 		}
